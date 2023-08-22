b8cc74cb6ca25a909d1bbc1499fbe63c9db2751d
Geoffrey Bii <geoffrey.bii@student.moringaschool.com>
2023-08-22 15:17:12 +0300

    submit assignment

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,12 @@
-# your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word_char = sorted([char for char in word])
+
+    def match(self, word_list):
+        matche = [word for word in word_list if sorted([char for char in word]) == self.word_char]
+
+        return matche
+          
+         
+
+    
